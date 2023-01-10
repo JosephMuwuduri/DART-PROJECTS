674c1610a1ebf1737c30faa25218e76e5c397a84
Joseph K Muwuduri <94116756+JosephMuwuduri@users.noreply.github.com>
2023-01-10 06:56:08 +0100

    Add files via upload

---
+++ LAB4.dart
@@ -0,0 +1,58 @@
+//NAME   : JOSEPH MUWUDURI
+//SUBJECT: OOP
+//TASK   : Design simple domain in OOP using DART classes a)Classes should have some attributes and methods b) Use class c) Use override toString()
+
+class University {
+  String name;
+  List<Department> departments;
+
+  University(this.name, this.departments);
+}
+
+class Department {
+  String name;
+  List<Course> courses;
+
+  Department(this.name, this.courses);
+}
+
+class Course {
+  String name;
+  int credits;
+
+  Course(this.name, this.credits);
+}
+
+class Student {
+  String name;
+  List<Course> courses;
+
+  Student(this.name, this.courses);
+
+  @override
+  String toString() {
+    return 'Student: $name, Courses: $courses';
+  }
+}
+
+void main() {
+  // Create a list of courses
+  var courses = [
+    Course('Math', 3),
+    Course('Science', 4),
+    Course('History', 2)
+  ];
+
+  // Create a list of departments
+  var departments = [
+    Department('Arts', courses),
+    Department('Sciences', courses)
+  ];
+
+  // Create a university
+  var university = University('My University', departments);
+
+  // Create a student
+  var student = Student('John Doe', courses);
+  print(student);
+}
---
+++ LAB6.dart
@@ -0,0 +1,68 @@
+abstract class Vehicle {
+  // Abstract class for the core concept of a vehicle
+  int _maxSpeed;
+  int _seats;
+  String _type;
+
+  void setMaxSpeed(int speed) => _maxSpeed = speed;
+  void setSeats(int seats) => _seats = seats;
+  void setType(String type) => _type = type;
+
+  int getMaxSpeed() => _maxSpeed;
+  int getSeats() => _seats;
+  String getType() => _type;
+
+  void startEngine() => print('Engine started');
+  void stopEngine() => print('Engine stopped');
+}
+
+class Car extends Vehicle {
+  // Inheritance: Car class extends the Vehicle class
+  int _gears;
+
+  void setGears(int gears) => _gears = gears;
+  int getGears() => _gears;
+
+  void shiftGearUp() => print('Shifting gear up');
+  void shiftGearDown() => print('Shifting gear down');
+}
+
+class PremiumCarFabric {
+  // Fabric class for creating premium cars
+  static Car createCar(int maxSpeed, int seats, String type) {
+    Car car = Car();
+    car.setMaxSpeed(maxSpeed);
+    car.setSeats(seats);
+    car.setType(type);
+    car.setGears(6); // Premium cars come with 6 gears
+    return car;
+  }
+}
+
+class BasicCarFabric {
+  // Fabric class for creating basic cars
+  static Car createCar(int maxSpeed, int seats, String type) {
+    Car car = Car();
+    car.setMaxSpeed(maxSpeed);
+    car.setSeats(seats);
+    car.setType(type);
+    car.setGears(5); // Basic cars come with 5 gears
+    return car;
+  }
+}
+
+void main(List<String> args) {
+  // Parse command line arguments for car configuration
+  int maxSpeed = int.parse(args[0]);
+  int seats = int.parse(args[1]);
+  String type = args[2];
+
+  // Create a car using the fabric classes
+  Car car = PremiumCarFabric.createCar(maxSpeed, seats, type);
+
+  // Use getters to print some details about the car
+  print('Max speed: ${car.getMaxSpeed()}');
+  print('Seats: ${car.getSeats()}');
+  print('Type: ${car.getType()}');
+  print('Gears: ${car.getGears()}');
+}
